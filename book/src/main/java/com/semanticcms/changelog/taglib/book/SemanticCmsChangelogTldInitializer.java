806180cbd1eea4bebc320dfbfa5c60b2a6cf5124
AO Industries, Inc <support@aoindustries.com>
2019-04-15 19:33:35 -0500

    Fixed cross-project linking of javadocs (really this time).
    The issue was our redirects from */apidocs/ to */apidocs/index.html
    were interacting with a Maven plugin bug:
    
    [MJAVADOC-539] Upgrading plugin from 3.0.0 to 3.0.1 and 3.1.0 breaks certain external javadoc links
    
    https://issues.apache.org/jira/browse/MJAVADOC-539

--- book/src/main/java/com/semanticcms/changelog/taglib/book/SemanticCmsChangelogTldInitializer.java
+++ book/src/main/java/com/semanticcms/changelog/taglib/book/SemanticCmsChangelogTldInitializer.java
@@ -31,9 +31,9 @@ public class SemanticCmsChangelogTldInitializer extends TagReferenceInitializer 
 	private static final Map<String,String> additionalApiLinks = new LinkedHashMap<String,String>();
 	static {
 		// Self
-		additionalApiLinks.put("com.semanticcms.changelog.taglib.", Maven.properties.getProperty("documented.url") + "apidocs");
+		additionalApiLinks.put("com.semanticcms.changelog.taglib.", Maven.properties.getProperty("documented.url") + "apidocs/");
 		// Dependencies
-		additionalApiLinks.put("com.semanticcms.core.model.", "https://semanticcms.com/core/model/apidocs");
+		additionalApiLinks.put("com.semanticcms.core.model.", "https://semanticcms.com/core/model/apidocs/");
 	}
 
 	public SemanticCmsChangelogTldInitializer() {
