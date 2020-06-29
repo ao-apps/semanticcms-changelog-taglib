64a00a2575fd9874a213b5c5bd6b8eefa677be57
AO Industries, Inc <support@aoindustries.com>
2020-06-29 00:16:37 -0500

    Fixed bug for absolute links to elements on the same page.

--- src/main/java/com/semanticcms/changelog/taglib/ReleaseTag.java
+++ src/main/java/com/semanticcms/changelog/taglib/ReleaseTag.java
@@ -110,12 +110,14 @@ public class ReleaseTag extends SimpleTagSupport {
 		String scmUrl,
 		boolean isSnapshot,
 		String tagName,
-		String id
+		String id,
+		boolean absolute
 	) throws IOException, ServletException, SkipPageException {
 		if(captureLevel == CaptureLevel.BODY) {
 			print("<ul>\n"
 				+ "<li>");
-			new Link().element(id).invoke(() -> {
+			// TODO: Remove absolute here, and have absolute automatically added in RssServlet?
+			new Link().element(id).absolute(absolute).invoke(() -> {
 				print(isSnapshot ? "Snapshot Notes" : "Release Notes");
 			});
 			print("</li>\n");
@@ -274,7 +276,8 @@ public class ReleaseTag extends SimpleTagSupport {
 							scmUrl,
 							isSnapshot,
 							tagName,
-							id
+							id,
+							true
 						);
 					});
 					new Nav(isSnapshot ? "Snapshot Links" : "Release Links").id("release-links-" + version).invoke(() -> {
@@ -287,7 +290,8 @@ public class ReleaseTag extends SimpleTagSupport {
 							scmUrl,
 							isSnapshot,
 							tagName,
-							id
+							id,
+							false
 						);
 					});
 					JspFragment body = getJspBody();
