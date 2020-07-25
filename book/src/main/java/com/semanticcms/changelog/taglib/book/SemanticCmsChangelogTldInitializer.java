1c768627785f2eb0bbe1c9db49f847767e0ade3f
AO Industries, Inc <support@aoindustries.com>
2020-07-25 14:53:04 -0500

    Removed unexpected "Book" in tag reference page titles

--- book/src/main/java/com/semanticcms/changelog/taglib/book/SemanticCmsChangelogTldInitializer.java
+++ book/src/main/java/com/semanticcms/changelog/taglib/book/SemanticCmsChangelogTldInitializer.java
@@ -33,7 +33,7 @@ public class SemanticCmsChangelogTldInitializer extends TagReferenceInitializer 
 
 	public SemanticCmsChangelogTldInitializer() throws ValidationException {
 		super(
-			Maven.properties.getProperty("project.name") + " Reference",
+			Maven.properties.getProperty("documented.name") + " Reference",
 			"Taglib Reference",
 			new ResourceRef(
 				new BookRef(
